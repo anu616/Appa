4eb44cee7fa175dd8925d83634b1ec67cdb8cf93
nush55 <anusha.tiwari16@gmail.com>
2021-05-21 16:28:26 -0400

    mute roles

--- Commands/channel.js
+++ Commands/channel.js
@@ -1,9 +1,26 @@
 function mute(args, receivedMessage) {
     let userID = args[0].substr(3, 18)
+    let reason = ""
+    for (let i = 1; i < args.length; i++) {
+        reason += args[i] + " "
+        
+    }
     let muteCatID = "741158534623920168"
     let name = "🧊・cooler-" + receivedMessage.guild.members.cache.get(userID).user.username
     
     receivedMessage.guild.members.cache.get(userID).roles.add("729667375804448818")
+    
+    //receivedMessage.member.roles.cache.has(verification)
+    let roles = ["726653869102334034", "782321346356314162", "785411730376622080", "824102091483512862", 
+                "715814746233896970", "738749980579463248", "740739487247630367", "780461463563141120", 
+                "715828863623495770", "715828869063507988", "725052066308816937", "725052062546788373",
+                "725052064362922096", "725052068007641201"]
+    
+    for (const r in roles) {
+        if (receivedMessage.guild.members.cache.get(userID).roles.cache.has(r)) {
+            receivedMessage.guild.members.cache.get(userID).roles.remove(r)
+        }
+    }
 
     createChannel(receivedMessage, muteCatID, name, "mute", userID)
 }
@@ -35,7 +52,7 @@ function createChannel(message, category, name, kind, userID) {
             ])
 
             madeChannel.send("<@" + userID + "> You have been muted. \n" + 
-            "<@&715465955420930090> will now take necessary action")
+            "<@&715503417845350483> will now take necessary action")
 
         } /* else if(kind == "A") {
             madeChannel.overwritePermissions([
