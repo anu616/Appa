2263ebb596331d9fc1d4ef47578bce7a63b1b57d
nush55 <anusha.tiwari16@gmail.com>
2021-05-27 15:41:29 -0400

    bump embed

--- Commands/bump.js
+++ Commands/bump.js
@@ -27,32 +27,42 @@ function bumpMsg(bumpType, receivedMessage) {
     let bumpChannelID = "724837035319492658"            //avatar bumping
     let bumpChannel = receivedMessage.guild.channels.cache.get(bumpChannelID)
     let msg = ""
-    let embedMsg = new MessageEmbed()
+
+    let title = ""
+    let desc = ""
+    let col = ""
+    let thumb = ""
 
     if(bumpType == "!d bump") {
         msg = "<@&726161843793428562> Its time to bump the server!"
-        embedMsg.setTitle("Do !d bump")
-        embedMsg.setColor("0xD7E7F4")
-        embedMsg.setDescription("Do `!d bump` to bump! \n" + 
-        "After bumping, set a reminder using `appa bump !d bump`")
-        embedMsg.setThumbnail("https://cdn.discordapp.com/avatars/302050872383242240/67342a774a9f2d20d62bfc8553bb98e0.png?size=1024")
+        title = "Do !d bump"
+        col = "0xD7E7F4"
+        desc = "Do `!d bump` to bump! \n" + 
+        "After bumping, set a reminder using `appa bump !d bump`"
+        thumb = "https://cdn.discordapp.com/avatars/302050872383242240/67342a774a9f2d20d62bfc8553bb98e0.png?size=1024"
 
     } else if(bumpType == "!bump") {
         msg = "Its time to bump the server!"
-        embedMsg.setTitle("Do !bump")
-        embedMsg.setColor("0x25345C")
-        embedMsg.setDescription("Do `!bump` to bump! \n" + 
-        "After bumping, set a reminder using `appa bump !bump`")
-        embedMsg.setThumbnail("https://cdn.discordapp.com/avatars/315926021457051650/f90b3947729c79fc37e9ab9d0befc37f.png?size=1024")
+        title = "Do !bump"
+        col ="0x25345C"
+        desc ="Do `!bump` to bump! \n" + 
+        "After bumping, set a reminder using `appa bump !bump`"
+        thumb ="https://cdn.discordapp.com/avatars/315926021457051650/f90b3947729c79fc37e9ab9d0befc37f.png?size=1024"
 
     } else if(bumpType == "dlm!bump") {
         msg = "Its time to bump the server!"
-        embedMsg.setTitle("Do !bump")
-        embedMsg.setColor("0x748BDA")
-        embedMsg.setDescription("Do `dlm!bump` to bump! \n" + 
-        "After bumping, set a reminder using `appa bump dlm!bump`")
-        embedMsg.setThumbnail("https://cdn.discordapp.com/avatars/212681528730189824/8f501c0ea80824ce709f7adebfe11a94.png?size=1024")
+        title = "Do !bump"
+        col ="0x748BDA"
+        desc ="Do `dlm!bump` to bump! \n" + 
+        "After bumping, set a reminder using `appa bump dlm!bump`"
+        thumb ="https://cdn.discordapp.com/avatars/212681528730189824/8f501c0ea80824ce709f7adebfe11a94.png?size=1024"
     }
+
+    const embedMsg = new MessageEmbed()
+        .setTitle(title)
+        .setColor(col)
+        .setDescription(desc)
+        .setThumbnail(thumb)
 
     bumpChannel.send(msg)
     bumpChannel.send(embedMsg)
