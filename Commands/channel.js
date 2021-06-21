45f44d6139bfac0bae48f280e91faa1a40f5bee9
nush55 <anusha.tiwari16@gmail.com>
2021-06-21 13:56:52 -0400

    unmute reminder

--- Commands/channel.js
+++ Commands/channel.js
@@ -1,7 +1,6 @@
 import fs from 'fs'
-import { unmuteReminder } from './mute.js'
 
-function createChannel(receivedMessage, category, name, kind, userID, msg) {
+function createChannel(receivedMessage, category, name, kind, userID, msg, time, userTag) {
 
     receivedMessage.guild.channels.create(name, {
         type: "text",
@@ -37,6 +36,10 @@ function createChannel(receivedMessage, category, name, kind, userID, msg) {
             })
 
             madeChannel.send(msg)
+
+            if(time.length > 0) {
+				unmuteReminder(receivedMessage, madeChannel, time, userTag)
+			} 
 
         } else if(kind == "SW") {
             madeChannel.updateOverwrite("772880958902632488", {
@@ -104,7 +107,54 @@ function closeChannel(receivedMessage) {
     }
 }
 
+function unmuteReminder (receivedMessage, channel, time, userTag) {
+    let h = 0
+    let hpos = 0
+    let m = 0
+    let mpos = 0
+    let s = 0
+    let spos = 0
+    let l = 0
+    for (let i = 0; i < time.length; i++) {
+        if(time[i] == "h") {
+            hpos = i
+            h = time.substr(0, hpos)
+        } 
+        if (time[i] == "m") {
+            mpos = i
+            if(hpos == 0){
+                m = time.substr(0, mpos)
+            } else {
+                l = mpos - (hpos+1)
+                m = time.substr(hpos+1, l)
+            }
+        } 
+        if (time[i] == "s") {
+            spos = i
+            if(hpos == 0 && mpos == 0) {
+                s = time.substr(0, spos)
+            } else if (mpos == 0) {
+                l = spos - (hpos+1)
+                s = time.substr(hpos+1, l)
+            } else {
+                l = spos - (mpos+1)
+                s = time.substr(mpos+1, l)
+            } 
+        }
+    }
+    let ms = ((h*3600) + (m*60) + s) * 1000
 
+    setTimeout(() => {
+        let embedMsg = new MessageEmbed()
+            .setTitle("Mute duration up")
+            .setDescription("It is time to unmute " + userTag)
+            .setColor(receivedMessage.guild.roles.cache.get("715503417845350483").hexColor)
+
+        channel.send("<@&715503417845350483>")
+        channel.send(embedMsg)
+    }, ms);
+
+}
 
 
 export { createChannel, closeChannel as default }
--- Commands/mute.js
+++ Commands/mute.js
@@ -95,10 +95,7 @@ function mute(args, receivedMessage) {
 
             receivedMessage.channel.send("Muted " + userTag)
 
-            let madeChan = createChannel(receivedMessage, muteCatID, name, "mute", userID, muteMsg)
-            if(time.length > 0) {
-				unmuteReminder(receivedMessage, madeChan, time, userTag)
-			} 
+            let madeChan = createChannel(receivedMessage, muteCatID, name, "mute", userID, muteMsg, time, userTag)
             
         }
     } else {
@@ -125,53 +122,4 @@ function unmute(args, receivedMessage) {
     }
 }
 
-function unmuteReminder (receivedMessage, channel, time, userTag) {
-    let h = 0
-    let hpos = 0
-    let m = 0
-    let mpos = 0
-    let s = 0
-    let spos = 0
-    let l = 0
-    for (let i = 0; i < time.length; i++) {
-        if(time[i] == "h") {
-            hpos = i
-            h = time.substr(0, hpos)
-        } 
-        if (time[i] == "m") {
-            mpos = i
-            if(hpos == 0){
-                m = time.substr(0, mpos)
-            } else {
-                l = mpos - (hpos+1)
-                m = time.substr(hpos+1, l)
-            }
-        } 
-        if (time[i] == "s") {
-            spos = i
-            if(hpos == 0 && mpos == 0) {
-                s = time.substr(0, spos)
-            } else if (mpos == 0) {
-                l = spos - (hpos+1)
-                s = time.substr(hpos+1, l)
-            } else {
-                l = spos - (mpos+1)
-                s = time.substr(mpos+1, l)
-            } 
-        }
-    }
-    let ms = ((h*3600) + (m*60) + s) * 1000
-
-    setTimeout(() => {
-        let embedMsg = new MessageEmbed()
-            .setTitle("Mute duration up")
-            .setDescription("It is time to unmute " + userTag)
-            .setColor(receivedMessage.guild.roles.cache.get("715503417845350483").hexColor)
-
-        channel.send("<@&715503417845350483>")
-        channel.send(embedMsg)
-    }, ms);
-
-}
-
-export { mute, unmute, unmuteReminder }
+export { mute, unmute }
