b3cf63539d6a69698e033afe706f5943c404d0c2
nush55 <anusha.tiwari16@gmail.com>
2021-06-22 15:21:59 -0400

    changed bot channel

--- Commands/yipyip.js
+++ Commands/yipyip.js
@@ -1,22 +1,15 @@
 import { MessageEmbed } from 'discord.js'
 
 function yipYip(receivedMessage) {
-    let laws = "715480633463275597"
-    let map = "715843510875717634"
-    let roles = "750071527780778094"
-    let baSingSe = "723016761712050186"
-    let aangFanClub = "738743723692654642"
-    let paiSho = "725059189340242010" 
-    let bot = "794047813834899496"
     let emoji = "<a:_Bisons:746082006248456283>"
 
-    let msg = emoji + " Go to <#" + laws + "> for the Server Laws! \n" +
-    emoji + " Go to <#" + map + "> for the Channel Directory! \n" + 
-    emoji + " Go to <#" + roles + "> to choose Roles! \n" +
-    emoji + " Go to <#" + baSingSe + "> for the Main Text Channel! \n" +
-    emoji + " Go to <#" + aangFanClub + "> for the Avatar Text Channel! \n" +
-    emoji + " Go to <#" + paiSho + "> for the Event Text Channel! \n" +
-    emoji + " Go to <#" + bot + "> to play with our Bots! "
+    let msg = emoji + " Go to <#715480633463275597> for the Server Laws! \n" +
+    emoji + " Go to <#715843510875717634> for the Channel Directory! \n" + 
+    emoji + " Go to <#750071527780778094> to choose Roles! \n" +
+    emoji + " Go to <#723016761712050186> for the Main Text Channel! \n" +
+    emoji + " Go to <#738743723692654642> for the Avatar Text Channel! \n" +
+    emoji + " Go to <#725059189340242010> for the Event Text Channel! \n" +
+    emoji + " Go to <#715495520730349578> to play with our Bots! "
 
     embedMsg(msg, receivedMessage)
 }
