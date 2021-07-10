9331db64a8430b88be970732f8af274c85526fb0
nush55 <anusha.tiwari16@gmail.com>
2021-07-09 21:36:07 -0400

    dm reply

--- Commands/dm.js
+++ Commands/dm.js
@@ -1,7 +1,7 @@
-import { Client } from "discord.js"
+import { MessageEmbed } from "discord.js"
 
 
-function dm (args, receivedMessage) {
+function dm(args, receivedMessage) {
     if (receivedMessage.member.roles.cache.has("715503417845350483")) {
         let userID = ""
             if(args[0].startsWith("<@!")) {
@@ -16,11 +16,33 @@ function dm (args, receivedMessage) {
 
         user.send(msg)
 
+        let msgID = user.dmChannel.lastMessageID
+
+        dmReceive(user, msgID)
+       
         receivedMessage.delete()
     } else {
         receivedMessage.channel.send("You are not authorized to use this command")
     }
     
+}
+
+function dmReceive(user, msgID) {
+    let msgs =  user.dmChannel.messages.get(after, msgID) 
+
+    let msgDesc = "" 
+
+    msgs.array.forEach(msg => {
+        msgDesc += msg.createdAt + "\n"
+        + msg.content + "\n\n"
+    });
+
+    const embedMsg = new MessageEmbed()
+        .setTitle(user.tag)
+        .setThumbnail(user.defaultAvatarURL)
+        .setDescription(msgDesc)
+
+    receivedMessage.guild.members.cache.get("661015948249268272").send(embedMsg)
 }
 
 export { dm as default }
