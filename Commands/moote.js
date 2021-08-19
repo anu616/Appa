6d26eecdcb7ef7bcb61a73b5b52546ca87e4cfac
Anu <70354936+anu616@users.noreply.github.com>
2021-08-19 15:44:33 +0000

    moote

--- Commands/moote.js
+++ Commands/moote.js
@@ -5,20 +5,25 @@ function moote (args, receivedMessage) {
 	} else {
 		if(receivedMessage.member.roles.cache.has('868861213684158485')) {
 
-			let userID = receivedMessage.author.id
-			let roles = ['839264531279904838', '868861213684158485']
+			if (contains("senior", args[0]) || contains("staff", args[0])) {
+				let userID = receivedMessage.author.id
+				let roles = ['839264531279904838', '868861213684158485']
 
-			if(contains("senior", args[0]) && (receivedMessage.member.roles.cache.has('873197679964983376'))) {
-				roles.push('873197679964983376')
+				if(contains("senior", args[0]) && (receivedMessage.member.roles.cache.has('873197679964983376'))) {
+					roles.push('873197679964983376')
 
-			} 
-			roles.forEach(id => {
-        		receivedMessage.guild.members.cache.get(userID).roles.remove(id)
-    		})
+				} 
+				roles.forEach(id => {
+        			receivedMessage.guild.members.cache.get(userID).roles.remove(id)
+    			})
 
-			receivedMessage.guild.members.cache.get(userID).roles.add('877715983081553961')
+				receivedMessage.guild.members.cache.get(userID).roles.add('877715983081553961')
 
-			receivedMessage.channel.send("Mooted discord by yeeting your ping roles. Good luck with whatever youre doing! <:CabbageBlush3:869753338714005514>")
+				receivedMessage.channel.send("Mooted discord by yeeting your ping roles. Good luck with whatever youre doing! <:CabbageBlush3:869753338714005514>")
+
+			} else {
+				receivedMessage.channel.send("Appa doesnt understand your funny words, magic dumbass")
+			}
 			
 		} else {
 			receivedMessage.channel.send("You are not authorized to use this command")
@@ -31,23 +36,27 @@ function unmoote (args, receivedMessage) {
 		receivedMessage.channel.send("Please rerun the command as `Appa unmoote <position>` based on your position as one of the following: \n"
 		 + "Senior, Staff")
 	} else {
-		if(receivedMessage.member.roles.cache.has('868861213684158485')) {
+		if(receivedMessage.member.roles.cache.has('875697458791993384')) {
 
-			let userID = receivedMessage.author.id
-			let roles = ['839264531279904838', '868861213684158485']
+			if (contains("senior", args[0]) || contains("staff", args[0])) {
+				let userID = receivedMessage.author.id
+				let roles = ['839264531279904838', '868861213684158485']
 
-			if(contains("senior", args[0]) && (receivedMessage.member.roles.cache.has('873197679964983376'))) {
+				if(contains("senior", args[0]) && (receivedMessage.member.roles.cache.has('873109513820078090'))) {
 				roles.push('873197679964983376')
 
-			} 
-			roles.forEach(id => {
+				} 
+				roles.forEach(id => {
         		receivedMessage.guild.members.cache.get(userID).roles.add(id)
-    		})
+    			})
 
-			receivedMessage.guild.members.cache.get(userID).roles.remove('877715983081553961')
+				receivedMessage.guild.members.cache.get(userID).roles.remove('877715983081553961')
 
-			receivedMessage.channel.send("Welcome back to the world of pings, your discord has been unmooted <:bihelllord:869749081558351933>")
+				receivedMessage.channel.send("Welcome back to the world of pings, your discord has been unmooted <:Ung:746656481755136070>")
 			
+			} else {
+				receivedMessage.channel.send("Appa doesnt understand your funny words, magic dumbass")
+			}
 		} else {
 			receivedMessage.channel.send("You are not authorized to use this command")
 		}
