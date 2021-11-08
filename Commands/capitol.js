f630924c9de22574e9a67ff18ee9cf6dec0c01d0
anu616 <anusha.tiwari16@gmail.com>
2021-11-07 23:03:15 -0500

    capitol

--- Commands/capitol.js
+++ Commands/capitol.js
@@ -7,9 +7,45 @@ function add(recievedMessage, args) {
             "Note: This command requires you to have the Capitol role.")
 		} else {
 			let roleID = args[0].substr(3, 18)
-			for(int i = 1, i < args.length, i++) {
-				
+			let userID = "";
+			for (let i = 1; i < args.length; i++) {
+				if(args[1].startsWith("<@!")) {
+					userID = args[0].substr(3, 18)
+				}
+				else {
+					userID = args[0].substr(2, 18)
+				}
+				receivedMessage.guild.members.cache.get(userID).roles.add(roleID)				
 			}
+			recievedMessage.channel.send("Role added succesfully")
 		}
+	} else {
+		receivedMessage.channel.send("You are not allowed to use this command\n")
+	}
+}
+
+function remove(recievedMessage, args) {
+	if(receivedMessage.member.roles.cache.has("868861213684158485")) {
+        if(args.length < 2) {
+			recievedMessage.channel.send("Please run the command in the following format: \n"
+            + "Enter the role to be removed, and the users you wish to add the role too. \n"
+            + "`appa remove [@Role] [@User1] [@User2]...`\n" +
+            "Note: This command requires you to have the Capitol role.")
+		} else {
+			let roleID = args[0].substr(3, 18)
+			let userID = "";
+			for (let i = 1; i < args.length; i++) {
+				if(args[1].startsWith("<@!")) {
+					userID = args[0].substr(3, 18)
+				}
+				else {
+					userID = args[0].substr(2, 18)
+				}
+				receivedMessage.guild.members.cache.get(userID).roles.remove(roleID)				
+			}
+			recievedMessage.channel.send("Role removed succesfully")
+		}
+	} else {
+		receivedMessage.channel.send("You are not allowed to use this command\n")
 	}
 }
